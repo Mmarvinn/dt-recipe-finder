82dca990706057df8d3c967c61469dbef5437c49
Anatolii Tserkunyk <cerkunik@gmail.com>
2025-04-11 14:32:51 +0300

    add linter and prettier files

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  extends: ['next', 'prettier'],
+  plugins: ['prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+    'no-unused-vars': 'warn',
+    'no-console': 'warn',
+  },
+};
